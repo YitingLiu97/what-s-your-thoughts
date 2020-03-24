d5ed5fbd6cbd377e54fbcd4309ef7a648001e58f
YitingLiu97 <54900403+YitingLiu97@users.noreply.github.com>
2020-03-24 16:35:40 -0400

    initiating

---
+++ client.js
@@ -0,0 +1,15 @@
+// window.addEventListener('DOMContentLoaded', async()=>{
+
+// });
+
+//based on the key pressed, adding several lines in different directions of the image 
+//color of the element is based on the length of the text 
+
+window.onload(e=>{
+
+    let textinput = document.getElementById('textinput');
+    let typing = document.createElement('input');
+    typing.setAttribute('value','text');
+    textinput.append(typing);
+
+});
---
+++ server.js
@@ -0,0 +1,2 @@
+//need it to save the input from the user through post 
+
